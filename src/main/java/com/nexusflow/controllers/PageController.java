ac68b523c3607ae83d32893b1d5592371219765a
WaseeKhan <mdvaseem014@gmail.com>
2025-08-25 23:55:05 +0530

    Validation on forms

--- src/main/java/com/nexusflow/controllers/PageController.java
+++ src/main/java/com/nexusflow/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.nexusflow.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.nexusflow.helpers.MessageType;
 import com.nexusflow.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class PageController {
@@ -76,21 +78,19 @@ public class PageController {
     }
 
     @PostMapping("/do-register")
-    public String doRegister(@ModelAttribute UserForm userForm, HttpSession session){
+    public String doRegister(@Valid @ModelAttribute UserForm userForm, BindingResult   rBindingResult, HttpSession session){
         System.out.println("Do Register Handler");
         // fetch form data
         // UserForm 
         System.out.println(userForm);
         // validate data 
+        if(rBindingResult.hasErrors()){
+            System.out.println("Error in validation: "+ rBindingResult.toString());
+            return "register";
+        }
+
         // save to db 
-        // User user = User.builder()
-        // .name(userForm.getName())
-        // .email(userForm.getEmail())
-        // .password(userForm.getPassword())
-        // .about(userForm.getAbout())
-        // .phoneNumber(userForm.getPhoneNumber())
-        // .profilePic("https://learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flcwd_logo.45da3818.png&w=1080&q=75")
-        // .build();
+
         User user = new User();
         user.setName(userForm.getName());
         user.setEmail(userForm.getEmail());
--- src/main/java/com/nexusflow/forms/UserForm.java
+++ src/main/java/com/nexusflow/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.nexusflow.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +18,19 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Name is required")
+    @Size(min=3,message = "Name must be at least 3 characters long")
     private String name;
+    
+    @Email(message = "Please provide a valid email address")
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min=3,message = "Password must be at least 3 characters long")
     private String password;
+    @NotBlank(message = "About is required")
     private String about; 
+    @Size(min=10, max=12, message = "Phone number must be between 10 and 12 characters")
     private String phoneNumber;  
 
 }
