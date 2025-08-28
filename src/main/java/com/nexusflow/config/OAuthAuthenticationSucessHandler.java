375a854993c8885b0a693cc26d67f74d627f16ec
WaseeKhan <mdvaseem014@gmail.com>
2025-08-28 17:02:17 +0530

    logo change & github login implemeted

--- src/main/java/com/nexusflow/config/OAuthAuthenticationSucessHandler.java
+++ src/main/java/com/nexusflow/config/OAuthAuthenticationSucessHandler.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -31,6 +32,57 @@ public class OAuthAuthenticationSucessHandler implements AuthenticationSuccessHa
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
 
+            logger.info("OAuthAuthenticationSucessHandler");
+
+            var oAuth2User  =  (DefaultOAuth2User)authentication.getPrincipal();
+            oAuth2User.getAttributes().forEach((key, value)->{
+                logger.info("{} : {}", key,value);
+            });
+            User user = new User();
+            user.setUserId(UUID.randomUUID().toString());
+            user.setRoleList(List.of(AppConstant.ROLE_USER));
+            user.setEnabled(true);
+            user.setEmailVerified(true);
+            user.setPassword("dont_know");
+
+            //identity of the provider
+          var OAuth2AuthenticationToken =  (OAuth2AuthenticationToken)authentication;
+          String authorizedClientRegistrationId = OAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+          logger.info("Authorized Client Registration Id : {}", authorizedClientRegistrationId);
+            
+
+          if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
+
+            user.setEmail(oAuth2User.getAttribute("email").toString());
+            user.setProfilePic(oAuth2User.getAttribute("picture").toString());
+            user.setName(oAuth2User.getAttribute("name").toString());
+            user.setProviderUserId(oAuth2User.getName());
+            user.setProvider(Providers.GOOGLE);
+            user.setAbout("This account is created from Google");
+           
+            
+          }else if(authorizedClientRegistrationId.equalsIgnoreCase("github")){
+            String email = oAuth2User.getAttribute("email") != null ? oAuth2User.getAttribute("email").toString() : oAuth2User.getAttribute("login").toString() + "@nexusflow.in";
+            String picture = oAuth2User.getAttribute("avatar_url").toString();
+            String name = oAuth2User.getAttribute("login").toString();
+            String providerUserId = oAuth2User.getName();
+            
+            user.setEmail(email);
+            user.setProfilePic(picture);
+            user.setName(name);
+            user.setProviderUserId(providerUserId); 
+            user.setProvider(Providers.GITHUB);
+            user.setAbout("This account is created from GitHub");
+
+            
+          } else {
+            logger.error("Login with {} is not supported", authorizedClientRegistrationId);
+            
+          }
+                
+
+/*
+
                 // get  data from google/github
             DefaultOAuth2User user =  (DefaultOAuth2User)authentication.getPrincipal();
 
@@ -41,7 +93,7 @@ public class OAuthAuthenticationSucessHandler implements AuthenticationSuccessHa
             });
 
             logger.info(user.getAuthorities().toString());
-            logger.info("OAuthAuthenticationSucessHandler");
+           
             String email = user.getAttribute("email").toString();
             String name = user.getAttribute("name").toString();
             String picture = user.getAttribute("picture").toString();
@@ -68,6 +120,19 @@ public class OAuthAuthenticationSucessHandler implements AuthenticationSuccessHa
                 
             }
             
+
+ */
+
+
+
+            User user2 = userRepository.findByEmail(user.getEmail()).orElse(null); 
+            
+
+            if (user2 ==null) {
+                userRepository.save(user);
+                logger.info("User saved:" +user.getEmail());
+                
+            }         
 
             new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
     }
