950e47b2a6f91a5ae4655808738a679a9bf556df
WaseeKhan <mdvaseem014@gmail.com>
2025-09-18 01:33:40 +0530

    feat: Sending email

---
+++ src/main/java/com/nexusflow/services/impl/EmailServiceImpl.java
@@ -0,0 +1,43 @@
+package com.nexusflow.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.nexusflow.services.EmailService;
+
+import jakarta.validation.Valid;
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.properties.domain_name}")
+    private String senderEmail;
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom(senderEmail);
+        mailSender.send(message);
+    }
+
+    @Override
+    public void sendEmailWithHtml() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendEmailWithHtml'");
+    }
+
+    @Override
+    public void sendEmailWithAttachment() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendEmailWithAttachment'");
+    }
+
+}
--- src/test/java/com/nexusflow/NexusflowApplicationTests.java
+++ src/test/java/com/nexusflow/NexusflowApplicationTests.java
@@ -1,13 +1,27 @@
 package com.nexusflow;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nexusflow.services.EmailService;
+
 
 @SpringBootTest
 class NexusflowApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private EmailService emailService;
+
+	@Test
+	void sendEmailTest() {
+		emailService.sendEmail("waseemk.aws@gmail.com", "Test From NexusFlow", 
+		"This is test email from NexusFlow application");
+		
 	}
 
 }
